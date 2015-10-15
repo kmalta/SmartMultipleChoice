244ab7217a38bfbcd328fda1d60c98565318b1e1
Kevin Malta <kevin.malta@gmail.com>
2015-10-15 13:13:44 -0700

    adds .pyc files to gitignore and adds stdout progress updates for data processing

--- scripts/multiple_choice_indico.py
+++ scripts/multiple_choice_indico.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append('../')
 sys.path.append('./')
-import secrets.py
+from secrets import *
 import word2vec_model_setup
 from indicoio import config, named_entities, keywords, text_tags
-
 
 config.api_key = INDICO_API_KEY
 
@@ -14,7 +13,7 @@ class ShortTextIndico(object):
         self.text = text
         self.entity_extraction(text)
         self.keywords_extraction(text)
-        self.text_tags(text)
+        self.text_tags_extraction(text)
 
     def entity_extraction(self, text):
         self.named_entities = named_entities(text)
@@ -29,44 +28,75 @@ class ShortTextIndico(object):
 class AnswerIndico(ShortTextIndico):
 
     def __init__(self, text, letter):
-        super().__init__(text)
+        ShortTextIndico.__init__(self, text)
         self.letter = letter
 
 
 class QuestionIndico(ShortTextIndico):
 
     def __init__(self, _id, question, correct_answer, answer_a_text, answer_b_text, answer_c_text, answer_d_text):
-        super().__init__(question)
+        ShortTextIndico.__init__(self, question)
+        self._id = _id
         self.question = question
-        self.question_id = _id
         self.correct_answer = correct_answer
-        self.answer_a = AnswerIndico(answer_a_text)
-        self.answer_b = AnswerIndico(answer_b_text)
-        self.answer_c = AnswerIndico(answer_c_text)
-        self.answer_d = AnswerIndico(answer_d_text)
+        self.answer_a = AnswerIndico(answer_a_text, 'A')
+        self.answer_b = AnswerIndico(answer_b_text, 'B')
+        self.answer_c = AnswerIndico(answer_c_text, 'C')
+        self.answer_d = AnswerIndico(answer_d_text, 'D')
 
 class DataSet(object):
 
-    def __init__(self, data_path):
+    def __init__(self, data_path, save_name):
         self.data_path = data_path
-        self.model = Word2Vec.load('../models/word2vec_model')
+        self.save_name = save_name
+        #self.model = Word2Vec.load('../models/word2vec_model')
         self.read_data()
 
     def read_data(self):
         questions_list = []
 
-        with open(data_path, 'r') as f:
+        with open(self.data_path, 'r') as f:
             lines = f.readlines()
 
+            length = len(lines)
+            idx = 0
             for line in lines[1:]:
+                idx += 1
+                if idx % 100 == 0:
+                    print "Processing:", idx, "out of", length
                 q = line.split('\t')
                 try: 
                     questions_list.append(QuestionIndico(q[0], q[1], q[2], q[3], q[4], q[5], q[6]))
                 except:
-                    print "ERROR: ", repr(q)
+                    print "ERROR reading question:", repr(q)
 
+        f.close()
         self.questions_list = questions_list
 
+    def save_object(self):
+        f = open('../models/' + self.save_name, 'w')
+        cPickle.dump(model, f, cPickle.HIGHEST_PROTOCOL)
+        f.close()
 
 
+#TEST FOR READING DATA
+
+data = DataSet('../../data_set/training_set.tsv', 'smarter_tha_8th_grader_training_set_data_classes.pickle')
+data.save_object
+
+for question in data.questions_list:
+    print "#################################################"
+    print "#################################################"
+    print "\n"
+    print "Question ID:", question._id
+    print "\n"
+    print "Quesion:", question.question
+    print "\n"
+    print "Answer A:", question.answer_a
+    print "Answer B:", question.answer_b
+    print "Answer C:", question.answer_c
+    print "Answer D:", question.answer_d
+    print "\n"
+    print "Correct Answer:", question.correct_answer
+    print "\n\n"
 
