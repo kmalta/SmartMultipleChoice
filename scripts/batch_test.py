f9e1d42c74ce485f6c35f4ff637f13c994af0b57
animekraxe <yukibhuynh@gmail.com>
2015-10-22 14:05:54 -0700

    Add experimental batch_test script

---
+++ scripts/batch_test.py
@@ -0,0 +1,24 @@
+from secrets import *
+import indicoio
+
+import shelve
+
+
+indicoio.config.api_key = INDICO_API_KEY
+
+class batch_tags(object):
+  def __init__(self, tags):
+    self.batch_tags = indicoio.sentiment(tags)
+
+  def print_batches(self):
+    print repr(self.batch_tags)
+
+def test_batch_tags():
+  return batch_tags(['Here is a sentence', 'Here is another sentence', 'This is a paragraph'])
+
+def add_to_shelf(key, value):
+  d = shelve.open('shelf_location')
+  d[key] = value
+  d.close()
+
+add_to_shelf('sentiments', test_batch_tags())
