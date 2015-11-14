d28bcf57dbc26f0eca0785e03321c4ec784c3a01
Angad Gill <angadsinghgill@gmail.com>
2015-11-14 11:45:21 -0800

    Bug fixes in models.py. Added a test script

--- MCTest/scripts/answerer/model.py
+++ MCTest/scripts/answerer/model.py
@@ -2,28 +2,30 @@ import gensim
 from scipy.spatial.distance import cosine
 import numpy as np
 
-from evaluation import *
+from MCTest.scripts.answerer import evaluation
 
 class Model(object):
 
   def __init__(self, **kwargs):
     self.trained = 0
-    self.stop_words = self.__stop_words_from_file()
+    self.stop_words = self._stop_words_from_file()
 
     pass
 
-  def __answer(self, story_question):
+  def _answer(self, story_question):
     """Must return answers for the four questions in one story, output format: ['A', 'A', 'A', 'A']"""
     pass
 
   def predict(self, data_set):
-    """Will call self.__answer on all stories in data_set and return a list of answer lists.
+    """Will call self._answer on all stories in data_set and return a list of answer lists.
     output format: [['A', 'A', 'A', 'A'], ...., ['A', 'A', 'A', 'A']]"""
-    self.train(data_set)
+    if not self.trained:
+      print "Model not trained"
+      return
     all_answers = []
     for story_question in data_set.story_question_list:
-      all_answers += [self.__answer(story_question)]
-      # break
+      all_answers += [self._answer(story_question)]
+      break
     return all_answers
 
   def evaluate(self, data_set, evaluations = None):
@@ -35,8 +37,8 @@ class Model(object):
       pass
     else:
       predictions = self.predict(data_set)
-      keys = model_eval_dict.keys() if evaluations == 'all' else evaluations
-      evals = [model_eval_dict[key](data_set, predictions) for key in keys]
+      keys = evaluation.model_eval_dict.keys() if evaluations == 'all' else evaluations
+      evals = [evaluation.model_eval_dict[key](data_set, predictions) for key in keys]
       for _eval in evals:
         _eval.evaluate
       self.statistics = [(key, _eval.statistic) for _eval in evals]
@@ -45,7 +47,7 @@ class Model(object):
     self.trained = 1
     pass
 
-  def __stop_words_from_file(self):
+  def _stop_words_from_file(self):
     """Reads the stop_words file and returns a list of stop_words"""
     try:
       stop_words_file = open('MCTest/scripts/answerer/stop_words.txt', 'r')
@@ -61,14 +63,25 @@ class WordVectorModel(Model):
     Model.__init__(self, **kwargs)
     self.word_model = word_model
 
-  def __tokenize(self, text, lower=True):
-    """Returns text which is tokenized, stipped, and lowered (optional)."""
+  def _tokenize(self, text, lower=True):
+    """Returns text which is tokenized, stripped, and lowered (optional)."""
     r = []
     for s in gensim.utils.tokenize(text, lower=lower):
       if s not in self.stop_words:
         r += [s]
     return ' '.join(r)
 
+  def _vectorize_and_add(self, text):
+    """ Converts text to vectors using self.word_model and adds all the vectors"""
+    words = text.split()
+    sum = np.zeros(self.word_model.vector_size)
+    for w in words:
+      try:
+        v = self.word_model[w]
+        sum += v
+      except:
+        pass
+    return sum
 
 
 
@@ -89,14 +102,15 @@ class Word2VecSentencePair(WordVectorModel):
 
   def __init__(self, word2vec_model, **kwargs):
     WordVectorModel.__init__(self, word2vec_model, **kwargs)
+    self.trained = 1
 
-  def __answer(self, story_question):
-    story_sentence_vectors = [self._vectorize_and_add(self.__tokenize(x)) for x in story_question.story.story_sentences]
+  def _answer(self, story_question):
+    story_sentence_vectors = [self._vectorize_and_add(self._tokenize(x)) for x in story_question.story.story_sentences]
     answers = []
     for question in story_question.questions_list:
       answer_min_cosines = []
       for answer in question.answers:
-        question_answer = self.__tokenize(question.question + ' ' + answer.answer)
+        question_answer = self._tokenize(question.question + ' ' + answer.answer)
         question_answer_vector = self._vectorize_and_add(question_answer)
         answer_cosines = []
         for story_sentence_vector in story_sentence_vectors:
---
+++ MCTest/scripts/test_models.py
@@ -0,0 +1,10 @@
+import gensim
+import dill
+from MCTest.scripts.answerer import model
+
+word_model = gensim.models.Word2Vec.load_word2vec_format('data/glove.6B.50d.txt')
+data_set = dill.load(open('MCTest/sharedObjects/dataSet_mc500_train.p', 'r'))
+
+# reload(model)
+m = model.Word2VecSentencePair(word_model)
+print m.evaluate(data_set, ['Accuracy'])
